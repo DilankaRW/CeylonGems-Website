cc992640a1d1f751c6263b7c537ae2a99869a179
VL.dilankarw <dilankarashmika10@gmail.com>
2024-10-20 22:54:59 +0530

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavbarStyles.css";
-import menuIcon from "./menubar.png";
+import menubar from "./gem-blue.png";
+import menuclose from "./gem-red.png";
 
 export const Navbar = () => {
+  const [menuImage, setMenuImage] = useState(menubar);
+  const [activeLink, setActiveLink] = useState();
+
+  const handleCheckboxChange = (event) => {
+    setMenuImage(event.target.checked ? menuclose : menubar);
+
+    // if (event.target.checked) {
+    //   setMenuImage(menuclose);
+    // } else {
+    //   setMenuImage(menubar);
+    // }
+  };
+
+  const handleLinkClick = (linkName) => {
+    setActiveLink(linkName);
+  };
+
   return (
     <div className="navcontainer">
       <nav>
-        <input type="checkbox" id="check" title="checkbox" />
-        <label for="check" className="checkbtn">
+        <input
+          type="checkbox"
+          id="check"
+          title="checkbox"
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="check" className="checkbtn">
           <i>
-            <img src={menuIcon} width="35px" alt="menu" />
+            <img src={menuImage} width="35px" alt="menu" />
           </i>
         </label>
         <label className="logo">Ceylon Gems</label>
         <ul>
           <li>
-            <a className="active">Home</a>
+            <a>Home</a>
           </li>
           <li>
             <a>Gems</a>
